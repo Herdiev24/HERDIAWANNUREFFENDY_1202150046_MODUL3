bb9c13c17083f3e3fb04c15da03a0208057d6d49
Herdiev24 <herdiawan2410@gmail.com>
2018-02-25 22:15:31 +0700

    First

---
+++ app/src/main/java/com/example/lenovog5080/herdiawannureffendy_1202150046_modul3/menulist.java
@@ -0,0 +1,29 @@
+package com.example.lenovog5080.herdiawannureffendy_1202150046_modul3;
+
+public class menulist {
+    //deklarasi variable yang akan digunakan
+    int gambar;
+    String nama;
+    String desc;
+    //method setter
+    public menulist(int gambar, String nama, String desc){
+        this.gambar=gambar;
+        this.nama=nama;
+        this.desc=desc;
+    }
+    //method getter untuk get gambar
+    public int getGambar() {
+        return gambar;
+    }
+
+    //method getter untuk get nama
+    public String getNama() {
+        return nama;
+    }
+
+    //method getter untuk get detail
+    public String getDesc() {
+        return desc;
+    }
+
+}
